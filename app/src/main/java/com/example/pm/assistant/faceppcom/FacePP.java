8976ab392ba662f7241c5ef4bf2af9023529480e
BOSS124 <igortrevelin@usp.br>
2018-06-21 09:21:29 -0300

    FaceSets utils implementation

---
+++ app/src/main/java/com/example/pm/assistant/faceppcom/FacePP.java
@@ -0,0 +1,7 @@
+package com.example.pm.assistant.faceppcom;
+
+public class FacePP {
+    public static String APIURL = "https://api-us.faceplusplus.com/facepp/v3";
+    public static String APIKEY = "citvp57CbL2qzjjG8YZKOjtFL7qxBgI-";
+    public static String APIKEYSECRET = "7wfQQFz73KCUFUqFzLLhsAkX9beDq4-S";
+}
---
+++ app/src/main/java/com/example/pm/assistant/faceppcom/FaceSetUtils.java
@@ -0,0 +1,166 @@
+package com.example.pm.assistant.faceppcom;
+
+import android.support.annotation.Nullable;
+import android.util.Base64;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class FaceSetUtils {
+    public static String create(@Nullable String faceSetName) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody;
+
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder();
+        bodyBuilder.setType(MultipartBody.FORM);
+        bodyBuilder.addFormDataPart("api_key", FacePP.APIKEY);
+        bodyBuilder.addFormDataPart("api_secret", FacePP.APIKEYSECRET);
+        if(faceSetName != null)
+            bodyBuilder.addFormDataPart("display_name", faceSetName);
+        requestBody = bodyBuilder.build();
+
+        Request request = new Request.Builder().url(FacePP.APIURL + "/faceset/create").post(requestBody).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            return jsonObject.getString("faceset_token");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean addFace(String fsToken, String faceToken) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody;
+
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder();
+        bodyBuilder.setType(MultipartBody.FORM);
+        bodyBuilder.addFormDataPart("api_key", FacePP.APIKEY);
+        bodyBuilder.addFormDataPart("api_secret", FacePP.APIKEYSECRET);
+        bodyBuilder.addFormDataPart("faceset_token", fsToken);
+        bodyBuilder.addFormDataPart("face_tokens", faceToken);
+        requestBody = bodyBuilder.build();
+
+        Request request = new Request.Builder().url(FacePP.APIURL + "/faceset/addface").post(requestBody).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            if(jsonObject.getInt("face_added") == 1)
+                return true;
+            else
+                return false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean removeFace(String fsToken, String faceToken) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody;
+
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder();
+        bodyBuilder.setType(MultipartBody.FORM);
+        bodyBuilder.addFormDataPart("api_key", FacePP.APIKEY);
+        bodyBuilder.addFormDataPart("api_secret", FacePP.APIKEYSECRET);
+        bodyBuilder.addFormDataPart("faceset_token", fsToken);
+        bodyBuilder.addFormDataPart("face_tokens", faceToken);
+        requestBody = bodyBuilder.build();
+
+        Request request = new Request.Builder().url(FacePP.APIURL + "/faceset/removeface").post(requestBody).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            if(jsonObject.getInt("face_removed") == 1)
+                return true;
+            else
+                return false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean delete(String fsToken) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody;
+
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder();
+        bodyBuilder.setType(MultipartBody.FORM);
+        bodyBuilder.addFormDataPart("api_key", FacePP.APIKEY);
+        bodyBuilder.addFormDataPart("api_secret", FacePP.APIKEYSECRET);
+        bodyBuilder.addFormDataPart("faceset_token", fsToken);
+        requestBody = bodyBuilder.build();
+
+        Request request = new Request.Builder().url(FacePP.APIURL + "/faceset/delete").post(requestBody).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            if(jsonObject.getString("faceset_token").equalsIgnoreCase(fsToken))
+                return true;
+            else
+                return false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static List<String> reconFaces(String fsToken, byte[] imgBytes) {
+        List<String> result = new ArrayList<>();
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody;
+
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder();
+        bodyBuilder.setType(MultipartBody.FORM);
+        bodyBuilder.addFormDataPart("api_key", FacePP.APIKEY);
+        bodyBuilder.addFormDataPart("api_secret", FacePP.APIKEYSECRET);
+        bodyBuilder.addFormDataPart("image_base64", Base64.encodeToString(imgBytes, Base64.NO_WRAP));
+        bodyBuilder.addFormDataPart("faceset_token", fsToken);
+        requestBody = bodyBuilder.build();
+
+        Request request = new Request.Builder().url(FacePP.APIURL + "/faceset/delete").post(requestBody).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            JSONArray resultsArray = jsonObject.getJSONArray("results");
+
+            for(int i = 0, len = resultsArray.length(); i < len; i++) {
+                result.add(resultsArray.getJSONObject(i).getString("face_token"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
